6c391fab39d40e4ead11732d1f93169ad1daa6ed
Nightdwarf26658 <alexandro01122005@gmail.com>
2025-04-08 15:00:56 +0300

    copypaste kolmio
    blaa bla blaa

--- Artillery/Artillery/Program.cs
+++ Artillery/Artillery/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Artillery
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace Artillery
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int width = 400;
+            int height = 400;
+            Vector2 A = new Vector2(width / 2, 0);
+            Vector2 B = new Vector2(0, height / 2);
+            Vector2 C = new Vector2(width, height * 3 / 4);
+
+            Raylib.InitWindow(600, 400, "Raylib Testi");
+            while (Raylib.WindowShouldClose() == false)
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Black);
+                Raylib.DrawLineV(A, B, Color.Green);
+                Raylib.DrawLineV(C, A, Color.Red);
+                Raylib.DrawLineV(B, C, Color.Yellow);
+
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
         }
     }
 }
