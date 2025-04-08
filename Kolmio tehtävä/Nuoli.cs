86c43a853ee194e9eb1b6c872e4af5f85300f8aa
Nightdwarf26658 <alexandro01122005@gmail.com>
2025-04-08 14:41:06 +0300

    Nuoli ?
    blaa blaa blaa

---
+++ Kolmio tehtävä/Nuoli.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raylib_test
+{
+    enum KärkiMateriaali
+    {
+        Puu,
+        Kivi,
+        Piikivi,
+        Rauta,
+        Kulta,
+        Timantti,
+    }
+
+
+    enum SulkaMateriaali
+    {
+        Lehti,
+        Kanansulka,
+        Kotkansulka,
+    }
+
+    internal class Nuoli
+    {
+        byte pituusCm;
+        SulkaMateriaali sulka;
+        KärkiMateriaali kärki;
+    }
+}
--- Kolmio tehtävä/Program.cs
+++ Kolmio tehtävä/Program.cs
@@ -7,20 +7,38 @@ namespace Raylib_test
     {
         static void Main(string[] args)
         {
-            int width = 400;
+            int width = 600;
             int height = 400;
             Vector2 A = new Vector2(width / 2,0);
             Vector2 B = new Vector2(0, height / 2);
             Vector2 C = new Vector2(width, height * 3 / 4);
 
-            Raylib.InitWindow(600, 400, "Raylib Testi");
+            List<Vector2> Dots = new List<Vector2>() { A, B, C,};
+
+            Vector2 Asuunta = new Vector2(1, 1);
+            Vector2 Bsuunta = new Vector2(1, 1);
+            Vector2 Csuunta = new Vector2(1, 1);
+
+            List<Vector2> Directions = new List<Vector2>() { Asuunta, Bsuunta, Csuunta,};
+
+            Raylib.InitWindow(width, height, "Raylib Testi");
             while(Raylib.WindowShouldClose() == false)
             {
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.Black);
-                Raylib.DrawLineV(A, B, Color.Green);
                 Raylib.DrawLineV(C, A, Color.Red);
                 Raylib.DrawLineV(B, C, Color.Yellow);
+
+                for (int i = 0; i < 3; i++)
+                {
+                    Raylib.DrawLineV(Dots[i], Dots[(i+1)%3], Color.Green);
+                    Dots[i] = Dots[i] + Directions[i] * 20 * Raylib.GetFrameTime();
+
+                    if (Dots[i].X > width || Dots[i].X < 0) { Directions[i] = new Vector2 (Directions[i].X * -1, Directions[i].Y); }
+
+                    if (Dots[i].Y > height || Dots[i].Y < 0) { Directions[i] = new Vector2(Directions[i].X,Directions[i].Y * -1); }
+                }
+                
 
                 Raylib.EndDrawing();
             }
